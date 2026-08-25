1b00618ae7ee98bb9e099776cab032ab0f18dcbc
wenli-wenli-wenli <108597902@qq.com>
2026-08-25 19:27:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-@app.route('/')
-
 app.config['SECRET_KEY'] = 'secret!'
-# 允许跨域，你的网页是github.io域名，后台是render域名，必须开
+
+# 允许跨域
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# 收到前端发来的chat消息，广播给全部人
+# 收到前端chat事件，广播所有人
 @socketio.on('chat')
 def handle_chat(text):
     print("收到消息：", text)
     socketio.emit('chat', text, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0")
+    # host 0.0.0.0 对外开放端口，render部署必须这样写
+    socketio.run(app, host="0.0.0.0", debug=False)
+
