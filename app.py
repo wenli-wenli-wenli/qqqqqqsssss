16be08002880728c036031ed37d61caf86d4946b
wenli-wenli-wenli <108597902@qq.com>
2026-08-25 19:42:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,15 @@ from flask_socketio import SocketIO
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
-# 允许跨域
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# 收到前端chat事件，广播所有人
+@app.route('/')
+def index():
+    return "chat backend ok"
+
 @socketio.on('chat')
 def handle_chat(text):
     print("收到消息：", text)
     socketio.emit('chat', text, broadcast=True)
 
-if __name__ == '__main__':
-    # host 0.0.0.0 对外开放端口，render部署必须这样写
-    socketio.run(app, host="0.0.0.0", debug=False)
-
+# 注意：这里不要写 socketio.run ！！
