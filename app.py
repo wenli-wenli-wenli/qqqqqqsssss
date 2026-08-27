d81dc0c9301687b42698cd58977df9274ff333f9
wenli-wenli-wenli <108597902@qq.com>
2026-08-27 09:50:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
-# 必须写在所有import最开头！！！
-from gevent import monkey
-monkey.patch_all()
-
 from flask import Flask
 from flask_socketio import SocketIO
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-
-# 强制指定async_mode为gevent
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="gevent")
-
-@app.route('/')
-def index():
-    return "chat backend ok"
-
+socketio = SocketIO(app,cors_allowed_origins="*")
 @socketio.on('chat')
-def handle_chat(text):
-    print("收到消息：", text)
-    socketio.emit('chat', text, broadcast=True)
-
-# 删掉 if __name__ == '__main__': socketio.run(...)
+def receive_chat(message):
+    socketio.emit('chat', message,broadcast=True)
+if __name__ == '__main__':
+    socketio.run(app,host='0.0.0.0',debug=False)
